828400eb69208f78a83d9ba21456752623623eb8
Sharath M <sharathdhanish@gmail..com>
2025-07-05 16:41:16 +0530

    frontend vercel json update

--- frontend/expense-tracker/src/utils/helper.js
+++ frontend/expense-tracker/src/utils/helper.js
@@ -1,62 +1,62 @@
-import moment from "moment";
+    import moment from "moment";
 
-export const validateEmail = (email) => {
-    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return regex.test(email);
-};
+    export const validateEmail = (email) => {
+        const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return regex.test(email);
+    };
 
-export const getInitials = (name) => {
-    if (!name) return "";
+    export const getInitials = (name) => {
+        if (!name) return "";
 
-    const words = name.split(" ");
-    let initials = "";
+        const words = name.split(" ");
+        let initials = "";
 
-    for (let i = 0;i< Math.min(words.length,2); i++) {
-        initials +=words[i][0];
+        for (let i = 0;i< Math.min(words.length,2); i++) {
+            initials +=words[i][0];
+        }
+
+        return initials.toUpperCase();
+    };
+
+    export const addThousandSeparator = (num) => {
+        if (num== null || isNaN(num)) return "";
+
+        const [integerPart, fractionalPart] = num.toString().split(".");
+        const formattedInteger = integerPart.replace(/\B(?=(\d{3})+(?!\d))/g,",");
+
+        return fractionalPart 
+        ? `${formattedInteger}.${fractionalPart}`
+        : formattedInteger;
+    };
+
+    export const prepareExpenseBarChartData = (data =[]) => {
+        const chartData = data.map((item) => ({
+            category: item?.category,
+            amount: item?.amount,
+        }));
+
+        return chartData;
     }
 
-    return initials.toUpperCase();
-};
+    export const prepareIncomeBarChartData = (data = []) => {
+        const sortedData = [...data].sort((a,b) => new Date(a.date) - new Date(b.date));
 
-export const addThousandSeparator = (num) => {
-    if (num== null || isNaN(num)) return "";
+        const chartData = sortedData.map((item) => ({
+            month: moment(item?.date).format('Do MMM'),
+            amount: item?.amount,
+            source: item?.source,
+        }));
 
-    const [integerPart, fractionalPart] = num.toString().split(".");
-    const formattedInteger = integerPart.replace(/\B(?=(\d{3})+(?!\d))/g,",");
+        return chartData;
+    }
 
-    return fractionalPart 
-    ? `${formattedInteger}.${fractionalPart}`
-    : formattedInteger;
-};
+    export const prepareExpenseLineChartData = (data = []) => {
+        const sortedData = [...data].sort ((a,b) => new Date(a.date) - new Date(b.date))
+        const chartData = sortedData.map((item) => ({
+            month: moment(item?.date).format('Do MMM'),
+            amount: item?.amount,
+            category: item?.category,
+        }));
 
-export const prepareExpenseBarChartData = (data =[]) => {
-    const chartData = data.map((item) => ({
-        category: item?.category,
-        amount: item?.amount,
-    }));
-
-    return chartData;
-}
-
-export const prepareIncomeBarChartData = (data = []) => {
-    const sortedData = [...data].sort((a,b) => new Date(a.date) - new Date(b.date));
-
-    const chartData = sortedData.map((item) => ({
-        month: moment(item?.date).format('Do MMM'),
-        amount: item?.amount,
-        source: item?.source,
-    }));
-
-    return chartData;
-}
-
-export const prepareExpenseLineChartData = (data = []) => {
-    const sortedData = [...data].sort ((a,b) => new Date(a.date) - new Date(b.date))
-    const chartData = sortedData.map((item) => ({
-        month: moment(item?.date).format('Do MMM'),
-        amount: item?.amount,
-        category: item?.category,
-    }));
-
-    return chartData;
-}
+        return chartData;
+    }
