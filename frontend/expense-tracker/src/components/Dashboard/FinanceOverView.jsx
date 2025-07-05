4bb9e02d48849614660d180c8242dfb761116afe
Sharath M <sharathdhanish@gmail..com>
2025-07-05 16:22:19 +0530

    inital message

---
+++ frontend/expense-tracker/src/components/Dashboard/FinanceOverView.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import CustomPieChart from '../Charts/CustomPieChart';
+
+
+const COLORS = ["#875CF5","#FA2C37", "#FF6900"];
+
+const FinanceOverView = ({totalBalance, totalIncome, totalExpense}) => {
+
+    const balanceData = [
+        {name: "Total Balance", amount:totalBalance},
+        {name: "Total Expense", amount:totalExpense},
+        {name: "Total Income", amount:totalIncome},
+    ];
+  return (
+    <div className='card'>
+        <div className='flex items-center justify-between'>
+            <h5 className='text-lg'>Financial Overview</h5>
+        </div>
+
+        <CustomPieChart
+            data={balanceData}
+            label="Total Balance"
+            totalAmount={`₹${totalBalance}`}
+            showTextAnchor={true} 
+            colors={COLORS}
+            
+        />
+    </div>
+  )
+}
+
+export default FinanceOverView
